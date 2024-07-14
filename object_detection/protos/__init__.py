cd6b1551bdfd2d16e87cf84d8aadbadc556de4dc
elizabeth26031999 <cuentacompartida1310@gmail.com>
2024-07-13 23:15:19 -0500

    initial

---
+++ object_detection/protos/__init__.py
@@ -0,0 +1,8 @@
+# from .eval_pb2 import *
+from .anchor_generator_pb2 import *
+from .string_int_label_map_pb2 import *
+# from .argmax_matcher_pb2 import *
+# from .box_coder_pb2 import *
+# from .calibration_pb2 import *
+# from .center_net_pb2 import *
+
